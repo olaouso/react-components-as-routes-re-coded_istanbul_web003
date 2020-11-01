7a609d5e9704c144731eb27b7dd8a1aba80f56fc
Ola Ouso <ouso.ola@gmail.com>
2020-11-01 12:54:19 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route, NavLink} from "react-router-dom"
 
+const link ={
+  width: "100px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "blue",
+  textDeoration: "none",
+  color: "white"
+}
+
+const Navbar =() =>
+  <div>
+    <NavLink
+      to="/" exact
+      style={link}
+      activeStyle={{
+        background: "darkblue"
+      }}
+      >Home</NavLink>
+      <NavLink
+      to="/about" exact
+      style={link}
+      activeStyle={{
+        background: "darkblue"
+      }}
+      >About</NavLink>
+      <NavLink
+      to="/login" exact
+      style={link}
+      activeStyle={{
+        background: "darkblue"
+      }}
+      >Login</NavLink>
+  </div>
 const Home = () => {
   return (
     <div>
